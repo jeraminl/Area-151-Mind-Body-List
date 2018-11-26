f3a06a6f55719b25cba4b73e7e07f3d0f15f80fd
Armando Torres <armant1@uci.edu>
2018-11-26 11:23:47 -0800

    firstRendition

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'list',
+    loadChildren: './list/list.module#ListPageModule'
+  },
+  { path: 'MindChecklist', loadChildren: './mind-checklist/mind-checklist.module#MindChecklistPageModule' },
+  { path: 'Settings', loadChildren: './settings/settings.module#SettingsPageModule' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/mind-checklist/mind-checklist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mind-checklist',
+  templateUrl: './mind-checklist.page.html',
+  styleUrls: ['./mind-checklist.page.scss'],
+})
+export class MindChecklistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
